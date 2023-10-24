02b6c795d396231b9d187781a1f36c04a95d3798
JAVIER BALLESTEROS JEREZ <Javier.Ballesteros4@alu.uclm.es>
2023-10-24 10:20:10 +0200

    Cambio en el seeder

--- db/seeder.ts
+++ db/seeder.ts
@@ -3,22 +3,28 @@ import Users, { User } from '@/models/User';
 import Orders, { Order } from '@/models/Order';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 
 dotenv.config({ path: `.env.local`, override: true });
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const products: Product[] = [
   {
-    name: 'Earthen Bottle',
+    name: 'Element Skateboard Starwars',
+    brand: 'Element',
+    size: '8.0 inches',
+    color: 'black',
     price: 39.95,
     img: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    description: 'What a bottle!',
   },
   {
-    name: 'Nomad Tumbler',
+    name: 'Nomad Skateboard',
+    brand: 'Nomad',
+    size: '8.25 inches',
+    color: 'green',
     price: 39.95,
     img: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    description: 'Yet another item',
   },
 ];
 
@@ -36,30 +42,31 @@ async function seed() {
 
   await conn.connection.db.dropDatabase();
 
-  /*const insertedProducts = await Products.insertMany(products);
+  const insertedProducts = await Products.insertMany(products);
 
   const orders: Order[] = [
     {
-      date: new Date(),
-      address: '123 Main St, 12345 New York, United States',
-      cardHolder: 'John Doe',
-      cardNumber: '1234 1234 1234 1234',
       orderItems: [
         {
           product: insertedProducts[0]._id,
           qty: 2,
-          price: products[0].price,
+          price: insertedProducts[0].price,
         }
-      ]
-    },
-
+      ],
+      date: new Date(Date.now()),
+      address: 'C/Felipe II, 2, 02600, Villarrobledo, Albacete',
+      cardHolder: 'John Doe',
+      cardNumber: '1234567890123456',
+    },    
   ];
-  
 
   const insertedOrders = await Orders.insertMany(orders);
+
+  const password = await bcrypt.hash('1234', 10);
+
   const user: User = {
     email: 'johndoe@example.com',
-    password: '1234',
+    password: password,
     name: 'John',
     surname: 'Doe',
     address: '123 Main St, 12345 New York, United States',
@@ -78,18 +85,31 @@ async function seed() {
       insertedOrders[0]._id,
     ],
   };
+  const user2: User = {
+    email: 'bornidoe@example.com',
+    password: password,
+    name: 'Borni',
+    surname: 'Doe',
+    address: '2 Felipe II, 02600 Villarrobledo, Spain',
+    birthdate: new Date('2002-04-01'),
+    cartItems: [
+      {
+        product: insertedProducts[0]._id,
+        qty: 2,
+      },
+      {
+        product: insertedProducts[1]._id,
+        qty: 5,
+      },
+    ],
+    orders: [
+      insertedOrders[0]._id,
+    ],
+  };
+
   const res = await Users.create(user);
-  console.log(JSON.stringify(res, null, 2));
-
-
-  const retrievedUser = await Users
-  .findOne({ email: 'johndoe@example.com' })
-  .populate('cartItems.product');
-  console.log(JSON.stringify(retrievedUser, null, 2));*/
-
-  await Products.insertMany(products);
-  await Users.createCollection();
-  await Orders.createCollection();
+  const res2 = await Users.create(user2);
+  console.log(JSON.stringify(res, null, 3));
 
   await conn.disconnect();
 }
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,31 +1,40 @@
 import mongoose, { Schema, Types } from 'mongoose';
 
 export interface Product {
-_id?: Types.ObjectId;
-name: string;
-description: string;
-img: string;
-price: number;
+  name: string;
+  brand: string;
+  size: string;
+  color: string;
+  img: string;
+  price: number;
 }
 
 const ProductSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    img: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: false,
+  },
+  size: {
+    type: String,
+    required: false,
+  },
+  color: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  img: {
+    type: String,
+    required: true,
+  },
 });
 
-export default mongoose.models.Product || mongoose.model<Product>('Product', ProductSchema);
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
