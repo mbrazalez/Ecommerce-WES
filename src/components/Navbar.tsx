45d809eedc38ebe0bb3f8811f1476598bc5296b9
JAVIER BALLESTEROS JEREZ <Javier.Ballesteros4@alu.uclm.es>
2023-11-11 17:41:12 +0100

    Create folders signup signin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ export default async function Navbar() {
                         ) : (
                             <>
                                 <Link
-                                    href='#'
+                                    href='/auth/signup'
                                     className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-gray-100'
                                 >
                                     Sign up
