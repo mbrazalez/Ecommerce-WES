a6017a37b1d4a736d5874d61cb6b7d6ffb5c3e51
mbrazalez <manu.brazalez@gmail.com>
2023-11-18 17:30:55 +0100

    [FIX] Color and size of cart button v2

--- src/app/(sections)/cart/page.tsx
+++ src/app/(sections)/cart/page.tsx
@@ -61,18 +61,18 @@ export default async function Cart() {
                                             <td className="px-6 py-4">
                                                 <div className="flex items-center">                         
                                                     <div className="inline-flex rounded-md shadow-sm " role="group" >
-                                                        <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 dark:bg-gray-400 border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                                        <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 dark:bg-gray-650 border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                                                             -
                                                         </button>
                                                         <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 dark:border-white dark:text-white dark:focus:bg-gray-700">
                                                             {cartItem.qty}
                                                         </button>
-                                                        <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 dark:bg-gray-400 border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                                        <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 dark:bg-gray-650 border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                                                             +
                                                         </button>
                                                     </div>
                                                     <div className="inline-flex rounded-md shadow-sm ml-2 ">
-                                                        <button type="button" className="inline-flex items-center px-2 py-2 text-gray-900 bg-gray-200 dark:bg-gray-400 border border-gray-900 rounded-lg hover:bg-red-600 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                                        <button type="button" className="inline-flex items-center px-2 py-2 text-gray-900 bg-red-200 dark:bg-red-400 border border-gray-900 rounded-lg hover:bg-red-600 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                                                 <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clipRule="evenodd" />
                                                             </svg>
