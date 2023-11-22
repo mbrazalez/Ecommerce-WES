e808b4d07d770f8697769a854c8410808eb0855a
JAVIER BALLESTEROS JEREZ <Javier.Ballesteros4@alu.uclm.es>
2023-11-22 20:29:42 +0100

    Add CartItemCounter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import React from 'react';
-import { CartItemsProvider } from '@/providers/cartItemProvider';
+import { CartItemsProvider } from '@/providers/cartItemsProvider';
 import { NextAuthProvider} from '@/providers/NextAuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
