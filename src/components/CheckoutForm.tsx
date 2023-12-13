81b5c17e4db8313782c9d7f2099c18b8f530941e
JAVIER BALLESTEROS JEREZ <Javier.Ballesteros4@alu.uclm.es>
2023-12-13 12:13:51 +0100

    Update Purchase button when it's disabled

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -43,6 +43,8 @@ export default function CheckoutForm() {
         }
     };
     
+    const isFormValid = formValues.address && formValues.cardHolder && formValues.cardNumber;
+    
     return (
         <>
             <form onSubmit={handleSubmit}>
@@ -106,7 +108,11 @@ export default function CheckoutForm() {
                     </div>
                 }
                 <div className="container px-10 mx-0 min-w-full flex flex-col items-center">
-                    <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    <button
+                        type="submit"
+                        disabled={!isFormValid}
+                        className={`text-white ${isFormValid ? 'bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300  dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700' : 'bg-gray-400'} font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 `}
+                    >
                         Purchase
                     </button>
                 </div>
